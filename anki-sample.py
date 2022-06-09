fb8e46fbc3f5a0ec7ffc5ddf21d1b6bc8d7b9e5b
lausek <root@lausek.eu>
2022-06-09 10:58:16 +0200

    extract deck database to same temporary location based on file path

--- anki-sample.py
+++ anki-sample.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 from dataclasses import dataclass
+from pathlib import Path
 from typing import List, Optional
 
 import logging
 import sqlite3
-import zipfile
 
 
 @dataclass
@@ -40,6 +40,21 @@ class Note:
             return None
 
 
+def load_db_from_deck(deck_path: Path) -> sqlite3.Connection:
+    from hashlib import md5
+    import tempfile
+    import zipfile
+
+    deck_digest = md5(str(deck_path).encode("utf-8")).hexdigest()
+    extracted_deck_name = f"anki-sample-{deck_digest}"
+    extracted_deck_path = Path(tempfile.gettempdir()) / extracted_deck_name
+
+    with zipfile.ZipFile(deck_path) as zf:
+        zf.extract("collection.anki2", extracted_deck_path)
+
+        return sqlite3.connect(extracted_deck_path / "collection.anki2")
+
+
 def load_questions(db) -> List[Note]:
     questions_raw = db.execute("select * from notes").fetchall()
     return [*filter(bool, map(Note.from_row, questions_raw))]
@@ -51,14 +66,7 @@ def rich_format_answer_md(answer: str) -> str:
     return markdownify(answer)
 
 
-def load_db_from_deck(deck_path: str) -> sqlite3.Connection:
-    with zipfile.ZipFile(deck_path) as zf:
-        zf.extract("collection.anki2")
-
-        return sqlite3.connect("collection.anki2")
-
-
-def sample_deck_for_n_notes(deck_path: str, request_n_samples: int):
+def sample_deck_for_n_notes(deck_path: Path, request_n_samples: int):
     import random
 
     from rich.console import Console
@@ -95,11 +103,12 @@ def main():
 
     args = parser.parse_args()
 
-    sample_deck_for_n_notes(args.DECK_PATH, args.samples)
+    sample_deck_for_n_notes(Path(args.DECK_PATH), args.samples)
 
 
 if __name__ == "__main__":
     from rich.logging import RichHandler
+
     logging.basicConfig(handlers=[RichHandler()])
 
     main()
